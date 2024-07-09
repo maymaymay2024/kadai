da1c0fa67f5b8b3590a67b45f41988ee76247768
maymaymay2024 <am0916.a.q.n.xxx.mayumink@gmail.com>
2024-07-09 22:46:32 +0900

    Update kadai.py

--- kadai.py
+++ kadai.py
@@ -13,39 +13,42 @@ option = st.selectbox(
     ('安産祈願', '金運上昇祈願', '長寿祈願')
 )
 
-# GitHubリポジトリのベースURL
-api_base_url = 'https://api.github.com/repos/maymaymay2024/kadai/contents/'
+# 各祈願の画像URLリスト
+image_urls = {
+    '安産祈願': [
+        'https://raw.githubusercontent.com/maymaymay2024/kadai/main/安産祈願/imageIMG_8470.JPG',
+        'https://raw.githubusercontent.com/maymaymay2024/kadai/main/安産祈願/imageIMG_8471.JPG',
+        'https://raw.githubusercontent.com/maymaymay2024/kadai/main/安産祈願/imageIMG_8472.JPG'
+    ],
+    '金運上昇祈願': [
+        'https://raw.githubusercontent.com/maymaymay2024/kadai/main/金運上昇祈願/imageIMG_2630.JPG',
+        'https://raw.githubusercontent.com/maymaymay2024/kadai/main/金運上昇祈願/imageIMG_2633.JPG',
+        'https://raw.githubusercontent.com/maymaymay2024/kadai/main/金運上昇祈願/imageIMG_2644.JPG'
+    ],
+    '長寿祈願': [
+        'https://raw.githubusercontent.com/maymaymay2024/kadai/main/長寿祈願/imageIMG_2928.JPG',
+        'https://raw.githubusercontent.com/maymaymay2024/kadai/main/長寿祈願/imageIMG_2933.JPG',
+        'https://raw.githubusercontent.com/maymaymay2024/kadai/main/長寿祈願/imageIMG_2936.JPG'
+    ]
+}
 
 # Goボタンが押されたとき
 if st.button('Go'):
-    # 選択された祈願に対応する画像フォルダを決定
-    if option == '安産祈願':
-        folder_url = api_base_url + '安産祈願'
-    elif option == '金運上昇祈願':
-        folder_url = api_base_url + '金運上昇祈願'
-    else:
-        folder_url = api_base_url + '長寿祈願'
+    # 選択された祈願に対応する画像URLリストを取得
+    selected_image_urls = image_urls.get(option, [])
 
-    # GitHub APIを使用してフォルダ内の画像ファイルリストを取得
-    response = requests.get(folder_url)
-    if response.status_code == 200:
-        files = response.json()
-        images = [file['download_url'] for file in files if file['name'].lower().endswith(('.png', '.jpg', '.jpeg'))]
+    # ランダムに画像を選択
+    if selected_image_urls:
+        selected_image_url = random.choice(selected_image_urls)
 
-        # ランダムに画像を選択
-        if images:
-            selected_image_url = random.choice(images)
+        # 画像をダウンロード
+        response = requests.get(selected_image_url)
+        if response.status_code == 200:
+            img = Image.open(BytesIO(response.content))
 
-            # 画像をダウンロード
-            response = requests.get(selected_image_url)
-            if response.status_code == 200:
-                img = Image.open(BytesIO(response.content))
-
-                # 画像を表示
-                st.image(img, caption=selected_image_url.split('/')[-1])
-            else:
-                st.error('画像のダウンロードに失敗しました。')
+            # 画像を表示
+            st.image(img, caption=selected_image_url.split('/')[-1])
         else:
-            st.error('指定されたフォルダに画像ファイルがありません。')
+            st.error('画像のダウンロードに失敗しました。')
     else:
-        st.error(f'フォルダの内容を取得できませんでした。URLを確認してください。 (ステータスコード: {response.status_code})')
+        st.error('指定された祈願の画像が見つかりません。')
